93f4945d7374608dd39434680f2533361812a5c5
evgeniy <kukushkinyawork@yandex.ru>
2025-01-24 15:47:32 +0300

    Added test Login->Verify

--- test/test_main.cc
+++ test/test_main.cc
@@ -2,10 +2,51 @@
 #include <drogon/drogon_test.h>
 #include <drogon/drogon.h>
 
-DROGON_TEST(BasicTest)
+//migrate -path=./migrations -database="postgres://postgres:mysecretpassword@localhost:5432/postgres?sslmode=disable" up
+DROGON_TEST(LoginAndVerify)
 {
-    // Add your tests here
+    using namespace drogon;
+    auto client = HttpClient::newHttpClient("http://localhost:5555");
+    auto req = HttpRequest::newHttpRequest();
+    req->setPath("/Login");
+    req->setMethod(Post);
+    req->setContentTypeCode(CT_APPLICATION_JSON);
+    Json::Value message;
+    message["phone_number"] = "8-999-999-99-99";
+    message["password"] = "password";
+    message["id_api"] = 1;
+    Json::FastWriter writer;
+    std::string str = writer.write(message);
+    req->setBody(str);
+    client->sendRequest(req, [TEST_CTX](ReqResult res, const HttpResponsePtr& resp) {
+        REQUIRE(res == ReqResult::Ok);
+        REQUIRE(resp != nullptr);
+        CHECK(resp->statusCode() == k200OK);
+        CHECK(resp->contentType() == CT_APPLICATION_JSON);
+        auto json = resp->getJsonObject();
+        REQUIRE(json != nullptr);
+        REQUIRE((*json)["token"].isString());
+        REQUIRE((*json)["id_user"].isInt());
+        auto req = HttpRequest::newHttpRequest();
+        req->setPath("/Verify");
+        req->setMethod(Post);
+        req->setContentTypeCode(CT_APPLICATION_JSON);
+        Json::Value message;
+        message["token"] = (*json)["token"].asString();
+        message["id_api"] = 1;
+        Json::FastWriter writer;
+        std::string str = writer.write(message);
+        req->setBody(str);
+        auto client = HttpClient::newHttpClient("http://localhost:5555");
+        client->sendRequest(req, [TEST_CTX](ReqResult res, const HttpResponsePtr& resp){
+            REQUIRE(res == ReqResult::Ok);
+            REQUIRE(resp != nullptr);
+            CHECK(resp->statusCode() == k200OK);
+        });
+    });
 }
+
+
 
 int main(int argc, char** argv) 
 {
